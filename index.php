a3609cdbfa984255ec6c933d3038c5c1277bb600
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-01 15:58:34 -0600

    added marsweather api js

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php require_once('layouts/header.php') ?>
     <body>
         <h1>Hello CIT 261</h1>
-        <button onclick="getLatestWeather()">Get Latest Weather</button>
+        <button onclick="requestData('http://phish.in/api/v1/songs/40.json')">Get Data</button>
     </body>
 <?php require_once('layouts/footer.php') ?>
--- js/marsweather_api.js
+++ js/marsweather_api.js
@@ -1,16 +1,17 @@
-    function ajax(url, callback) {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function(){
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-                callback(xmlhttp.responseText);
-            }
+function ajax(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            callback(xmlhttp.responseText);
         }
-        xmlhttp.open("GET", url, true);
-        xmlhttp.send();
     }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
 
-    function getLatestWeather() {
-        ajax('http://marsweather.ingenology.com/v1/latest/', function(data) {
-            console.log(data);
-        });
-    }
+function requestData(url) {
+    ajax(url, function(data) {
+        var json = JSON.parse(data);
+        console.log(json.data.title);
+    });
+}
