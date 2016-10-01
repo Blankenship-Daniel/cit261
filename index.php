bfe5731114f38446b722313589895a6cdf233712
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-01 15:24:52 -0600

    added marsweather api js

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php require_once('layouts/header.php') ?>
     <body>
         <h1>Hello CIT 261</h1>
+        <button onclick="getLatestWeather()">Get Latest Weather</button>
     </body>
 <?php require_once('layouts/footer.php') ?>
