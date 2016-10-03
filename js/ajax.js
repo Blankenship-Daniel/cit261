c9c37144a7247e5b50dda999ddcc0803b633efa7
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-03 17:21:09 -0600

    removed uncessary files from merge

--- js/ajax.js
+++ js/ajax.js
@@ -12,7 +12,6 @@ function ajax(url, callback) {
 function requestData(endpoint) {
     var root = 'https://api.github.com';
     ajax(root + endpoint, function(data) {
-        var json = JSON.parse(data);
-        console.log(json.data.title);
+        console.log(data);
     });
 }
