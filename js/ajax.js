64bc65f6da081dd9d7330776ccf66e54ed48bb0f
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-03 17:20:25 -0600

    removed uncessary files from merge

--- js/ajax.js
+++ js/ajax.js
@@ -6,13 +6,11 @@ function ajax(url, callback) {
         }
     }
     xmlhttp.open("GET", url, true);
-    xmlhttp.setRequestHeader('Access-Control-Allow-Origin', 'http://jsonplaceholder.typicode.com');
-    xmlhttp.setRequestHeader('Content-type', 'application/json');
     xmlhttp.send();
 }
 
 function requestData(endpoint) {
-    var root = 'http://jsonplaceholder.typicode.com/';
+    var root = 'https://api.github.com';
     ajax(root + endpoint, function(data) {
         var json = JSON.parse(data);
         console.log(json.data.title);
