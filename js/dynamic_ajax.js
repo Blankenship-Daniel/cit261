93515c6b1792a4a53d04943fb595a5085b614f03
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2016-10-03 17:42:35 -0600

    added dynamic ajax pages

---
+++ js/dynamic_ajax.js
@@ -0,0 +1,24 @@
+function ajax(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            callback(xmlhttp.responseText);
+        } else if (xmlhttp.status === 404) {
+            callback(404);
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function requestData(endpoint) {
+    var root = 'https://api.github.com';
+    ajax(root + endpoint, function(data) {
+        var output = document.getElementById('output').innerHTML;
+        if (data === 404) {
+            output = 'Sorry! We couldn\'t find that repo';
+        } else {
+            output = data;
+        }
+    });
+}
